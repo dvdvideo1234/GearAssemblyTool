9c7ca9fdbda8d5c906c37e0e898475327ba2e7f3
Deyan Dobromirov <dvd_video@abv.bg>
2015-02-01 09:37:27 +0200

    Life-spawn and logs

--- lua/gearassembly/gearasmlib.lua
+++ lua/gearassembly/gearasmlib.lua
@@ -75,8 +75,8 @@ local LibToolName = "gearassembly"
 
 -- Library Debug Settings. The file is created in
 -- the DATA folder :3 *.txt is appended
-local LibDebugEn = 1
-local LibLogFile = "" -- LibToolName .. "_log"
+local LibDebugEn = 0
+local LibLogFile = LibToolName .. "_log"
 local LibMaxLogs = 10000
 local LibCurLogs = 0
 
@@ -135,7 +135,7 @@ local LibSQLBuildError = ""
 local LibTables = {
   [LibTablePrefix.."PIECES"] = {
     Size = 7,
-    Keep = 5,
+    Keep = 3600,
     [1] = {"MODEL" , "TEXT", "L"},
     [2] = {"TYPE"  , "TEXT"},
     [3] = {"NAME"  , "TEXT"},
